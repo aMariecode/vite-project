5e04e47768cffa2d344a655228c22e2fb38bcd16
Patricia Marie Mandanas <patricialafaro9@gmail.com>
2025-02-06 21:56:19 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="main-container">
+      <div className="fullname">
+        Patricia Marie A. Mandanas
+      </div>
+      <div className="container">
+        CPEITEL
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
